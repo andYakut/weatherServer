de3bc926b4bebbdc249504c142804f3097148c91
andYakut <novahcek.mixa@gmail.com>
2019-07-24 09:45:31 +0300

    Add bcrypt and password comparison

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -1,27 +1,36 @@
+const { saltRounds } = require('../config/constants');
 const UserService = require('../services/user.services');
+const bcrypt = require('bcrypt');
 
 exports.createUser = async (req, res, next) => {
-  if(!req.body.username || !req.body.email && !req.body.password) {
-    // more validations
+  let newUser = {};
+  if (!req.body.username || !req.body.email || !req.body.password) {
     throw new Error('Some fields are empty')
-  } 
+  }
+
+  if (req.body.password !== req.body.confirmPassword) {
+    throw new Error("Passwords don't match");
+  }
 
   try {
-    const findUser = await UserService.findUser({email: req.body.email});
+    const findUser = await UserService.findUser({ email: req.body.email });
     if (findUser.length > 0) throw new Error('This user already exists')
-    
+
     // make hash of the password using bcrypt
-    
-    const newUser = {
+
+    const hash = await bcrypt.hash(req.body.password, saltRounds);
+    console.log(hash);
+
+    newUser = {
       email: req.body.email,
-      password: 'hashedPassword',
       username: req.body.username,
-      created: Date.now() 
+      password: hash,
+      created: Date.now()
     };
 
-    const createdUser = await UserService.createUser(newUser);
+    await UserService.createUser(newUser);
     return res.status(200).json({ status: 200, message: "User is successful created" });
-  } catch(e) {
+  } catch (e) {
     return res.status(400).json({ status: 400, message: e.message });
   }
 }
